523eaed03e9326e7e248dde243474352c091243f
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-03-19 05:32:08 +0300

    Special Template Features & Syntax

--- blog/views.py
+++ blog/views.py
@@ -1,41 +1,48 @@
 from django.shortcuts import render
 from datetime import date
 
-posts = [
+all_posts = [
   {
     "slug": "hike-in-the-mountains",
     "image": "mountains.jpeg",
     "author": 'Evgeniy',
     "date": date(2021, 3, 15),
     "title:": 'Mountain Hakking',
-    "exccerpt": "There's nothing like the views you get when hiking in the mountains!",
+    "excerpt": "There's nothing like the views you get when hiking in the mountains!",
     "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dicta eaque ex libero magnam molestiae quaerat quis quod reiciendis rem."
   },
   {
-    "slug": "hike-in-the-mountains",
-    "image": "mountains.jpeg",
+    "slug": "three",
+    "image": "three.jpeg",
     "author": 'Evgeniy',
     "date": date(2021, 3, 15),
-    "title:": 'Mountain Hakking',
-    "exccerpt": "There's nothing like the views you get when hiking in the mountains!",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dicta eaque ex libero magnam molestiae quaerat quis quod reiciendis rem."
+    "title:": 'Дерево',
+    "excerpt": "Ваш шедевр готов!",
+    "content": "Значимость этих проблем настолько очевидна, что новая модель организационной деятельности играет важную роль в формировании модели развития. Не следует, однако забывать, что укрепление и развитие структуры играет важную роль в формировании существенных финансовых и административных условий. Значимость этих проблем настолько очевидна, что консультация с широким активом обеспечивает широкому кругу (специалистов) участие в формировании систем массового участия."
   },
   {
-    "slug": "hike-in-the-mountains",
-    "image": "mountains.jpeg",
+    "slug": "iphone",
+    "image": "cosmos.jpeg",
     "author": 'Evgeniy',
     "date": date(2021, 3, 15),
-    "title:": 'Mountain Hakking',
-    "exccerpt": "There's nothing like the views you get when hiking in the mountains!",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate dicta eaque ex libero magnam molestiae quaerat quis quod reiciendis rem."
+    "title:": 'Iphone',
+    "excerpt": "Что-то новенькое!",
+    "content": "Равным образом укрепление и развитие структуры позволяет выполнять важные задания по разработке системы"
   }
 ]
+
+def get_date(post):
+  return post['date']
 
 # Create your views here.
 
 
 def starting_page(request):
-  return render(request, "blog/index.html")
+  sorted_posts = sorted(all_posts, key=get_date)
+  latest_posts = sorted_posts[-3:]
+  return render(request, "blog/index.html", {
+    "posts": latest_posts
+  })
 
 
 def posts(request):
