0ccd50d42927f9c541caced3059d0d3bd56d570b
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-03-24 08:41:08 +0300

    Comment Admin

--- blog/models.py
+++ blog/models.py
@@ -33,6 +33,9 @@ class Post(models.Model):
   author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
   tags = models.ManyToManyField(Tag)
 
+  def __str__(self):
+    return self.title
+
 
 class Comment(models.Model):
   user_name = models.CharField(max_length=120)
